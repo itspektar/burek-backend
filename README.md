63d4fd718c95234613e7fefe6016b12a17283f94
Sasa Kocic <sasa.kocic@yogaeasy.de>
2019-07-21 22:41:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Burek
+
+```bash
+docker build -t itspektar/burek:latest .
+docker-compose up -d
+curl localhost:5000
+Hello World! I have been seen 1 times.
+```
---
+++ app.py
@@ -0,0 +1,28 @@
+import time
+
+import redis
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+cache = redis.Redis(host='localhost', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
